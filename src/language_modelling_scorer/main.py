c915ddfed122dd98e8ce3c6876f4e5aa5306a851
Tommaso <p.tommaso@gmail.com>
2019-11-29 19:39:30 +0100

    now saving both loss and probability for each word

--- src/language_modelling_scorer/main.py
+++ src/language_modelling_scorer/main.py
@@ -4,7 +4,7 @@ from src.language_modelling_scorer.scorers import SentenceScorer
 
 
 def main(args):
-    scorer = SentenceScorer(args.model_name)
+    scorer = SentenceScorer(args.model_name, args.max_segments_per_batch)
     scorer.score_sentences(args.sentence_file, args.output_file)
 
 
@@ -13,5 +13,8 @@ if __name__ == "__main__":
     parser.add_argument("--sentence_file", required=True)
     parser.add_argument("--model_name", required=True, choices=["gpt2"])
     parser.add_argument("--output_file", required=True)
+    parser.add_argument("--max_segments_per_batch", default=800, type=int,
+                        desc="number of segments to include in a single batch.")
+
     args = parser.parse_args()
     main(args)
--- src/language_modelling_scorer/scorers.py
+++ src/language_modelling_scorer/scorers.py
@@ -6,13 +6,14 @@ from src.language_modelling_scorer.language_modelling import AutoHuggingfaceLM, 
 from src.language_modelling_scorer.lm_datasets import LMDataset
 import torch
 import allennlp.nn.util as nn_util
+import jsonlines
+
 
 class SentenceScorer(object):
     def __init__(self, model_name: str, max_seg_per_batch: int = 800):
         self.indexer = AutoHuggingfaceIndexer(model_name, use_starting_offsets=True)
         self.dataset = LMDataset(self.indexer)
         self.iterator = BucketIterator(
-            biggest_batch_first=True,
             sorting_keys=[("tokens", "num_tokens")],
             maximum_samples_per_batch=("tokens_length", max_seg_per_batch),
         )
@@ -24,7 +25,7 @@ class SentenceScorer(object):
                                            num_epochs=1,
                                            shuffle=False)
         counter = 0
-        with open(output_path, "w") as writer:
+        with jsonlines.open(output_path, mode="w") as writer:
             for batch in tqdm(raw_data_generator):
                 input_dict = batch["tokens"]
                 # if self.indexer.mask_token_id is not None:
@@ -36,39 +37,49 @@ class SentenceScorer(object):
                 #     input_dict["tokens"][torch.arange(input_dict["tokens"].size(0)), indices_offsets] = self.indexer.mask_token_id
                 batch = nn_util.move_to_device(batch, 0)
                 out_dict = self.model(**batch)
-                # all_logits = out_dict["scores"]
+                all_logits = out_dict["scores"]
                 metadata_list = batch["metadata"]
                 token_mapping = input_dict["tokens-offsets"]
                 all_input_tokens = input_dict["tokens"]
-                lines = self.get_output(all_input_tokens, metadata_list, token_mapping, out_dict["loss"].view(batch["tokens"]["tokens"].size(0), -1))
-                writer.write("\n".join(lines) + "\n")
+                outputs = self.get_output(all_input_tokens, all_logits, metadata_list, token_mapping,
+                                          out_dict["loss"].view(batch["tokens"]["tokens"].size(0), -1))
+                for o in outputs:
+                    writer.write(o)
                 counter += 1
 
-                # if counter == 100:
-                #     break
+                if counter == 100:
+                    break
 
-    def get_output(self, all_input_tokens, metadata_list, token_mapping, all_losses):
-        lines = list()
-        for mapping, metadata, input_tokens, losses in zip(token_mapping,
-                                                           metadata_list,
-                                                           all_input_tokens, all_losses):
+    def get_output(self, all_input_tokens, all_logits, metadata_list, token_mapping, all_losses):
+        outputs = list()
+        for mapping, metadata, input_tokens, losses, logits in zip(token_mapping,
+                                                                   metadata_list,
+                                                                   all_input_tokens, all_losses, all_logits):
             sid = metadata["sid"]
             indices = metadata["indices"]
             words = metadata["words"]
-            word_scores, perplexity = self.get_scores(input_tokens, losses, mapping, words, [x for x, _ in indices])
-            lines.append("{}\t{}\t{}".format(sid, perplexity, "\t".join(
-                ["{} {}".format(word.replace(" ", "_"), score) for word, score in word_scores])))
-        return lines
+            word_scores, perplexity = self.get_scores(input_tokens, logits, losses, mapping, words,
+                                                      [x for x, _ in indices])
+            outputs.append({"sentence_id": sid, "perplexity": perplexity,
+                            "words": [
+                                {"word": word.replace(" ", "_"), "loss": score, "probability": prob}
+                                for word, score, prob in
+                                word_scores]
+                            })
+        return outputs
 
-    def get_scores(self, input_ids, losses, mapping, words, words_indices):
-        # token_logits = logits[mapping]
+    def get_scores(self, token_ids, logits, losses, mapping, words, words_indices):
+        token_logits = logits[mapping[words_indices]]
+        word_seg_ids = token_ids[mapping[words_indices]]
+        probs = torch.softmax(token_logits, -1)
+        word_seg_scores = probs[torch.arange(probs.size(0)), word_seg_ids]
         # token_ids = input_ids[mapping]
         # token_preds = torch.softmax(token_logits, -1)
         # scores = token_preds[torch.arange(token_preds.size(0)), token_ids]
         words_seg_indices = mapping[words_indices]
         words_losses = losses[words_seg_indices]
         perplexity = torch.pow(2, -torch.mean(torch.log2(words_losses)))
-        return list(zip(words, [x.item() for x in words_losses])), perplexity
+        return list(zip(words, [x.item() for x in words_losses], [x.item() for x in word_seg_scores])), perplexity.item()
 
 
 if __name__ == "__main__":
