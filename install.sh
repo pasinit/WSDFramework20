85425eb4f42307de5be177ece02641a155f7cb84
tommaso <p.tommaso@gmail.com>
2020-12-02 11:30:16 +0100

    update

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cd WSDFramework20
 pip install git+https://github.com/pasinit/allennlp.git
 pip install git+https://github.com/pasinit/nlp_resources.git
 pip install pytorch_transformers==1.1.0
-pip install transformers==2.0.0
-pip install torch==1.2.0
+pip install transformers==2.1
+pip install torch==1.4.0
 pip install jsonlines
-eport PYTHONPATH=.
+export PYTHONPATH=.
